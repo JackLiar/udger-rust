5cd5fa0e8aacad09d556749669289d45c8ba8774
Jack Liar <>
2020-10-21 23:46:52 +0800

    remove unused type and trait

--- src/udger/regex_sequence.rs
+++ src/udger/regex_sequence.rs
@@ -20,28 +20,30 @@ impl RegexSequenceScratch {
     }
 }
 
-pub trait RegexSequenceTrait: Default {
+#[derive(Default)]
+pub struct RegexSequence {
+    pub name: String,
+    db: Option<Database>,
+    id_word_map: HashMap<u16, Vec<u16>>,
+    rowid_sequence_map: HashMap<u16, u16>,
+    rowid_id_map: HashMap<u16, u16>,
+}
+
+impl RegexSequence {
     fn new() -> Self {
         Self::default()
     }
 
-    fn db(&self) -> &Option<Database>;
+    fn db(&self) -> &Option<Database> {
+        &self.db
+    }
 
-    fn name(&self) -> &String;
+    fn name(&self) -> &String {
+        &self.name
+    }
 
-    fn rowid_sequence_map(&self) -> &HashMap<u16, u16>;
-
-    fn alloc_scratch(&self) -> Result<RegexSequenceScratch> {
-        match &self.db() {
-            None => Err(anyhow!(format!(
-                "RegexSequenceScratch {}'s database is None",
-                self.name()
-            ))),
-            Some(db) => Ok(RegexSequenceScratch::new(
-                self.name().clone(),
-                db.alloc_scratch()?,
-            )),
-        }
+    fn rowid_sequence_map(&self) -> &HashMap<u16, u16> {
+        &self.rowid_sequence_map
     }
 
     fn get_ids<T>(&self, ua: &T, scratch: &mut RegexSequenceScratch) -> Result<Vec<u16>>
@@ -84,32 +86,7 @@ pub trait RegexSequenceTrait: Default {
 
         Ok(id_seqs.iter().map(|(id, _)| *id).collect())
     }
-}
 
-#[derive(Default)]
-pub struct RegexSequence {
-    pub name: String,
-    db: Option<Database>,
-    id_word_map: HashMap<u16, Vec<u16>>,
-    rowid_sequence_map: HashMap<u16, u16>,
-    rowid_id_map: HashMap<u16, u16>,
-}
-
-impl RegexSequenceTrait for RegexSequence {
-    fn db(&self) -> &Option<Database> {
-        &self.db
-    }
-
-    fn name(&self) -> &String {
-        &self.name
-    }
-
-    fn rowid_sequence_map(&self) -> &HashMap<u16, u16> {
-        &self.rowid_sequence_map
-    }
-}
-
-impl RegexSequence {
     /// Initialize a RegexSequence
     pub fn init<'a, R, I, S>(
         &mut self,
@@ -219,112 +196,6 @@ impl RegexSequence {
             None => None,
             Some(id) => Some(*id),
         }
-    }
-}
-
-#[derive(Default)]
-pub struct DeviceBrandRegexSequence {
-    pub name: String,
-    db: Option<Database>,
-    rowid_code_map: HashMap<u16, [String; 2]>,
-    rowid_sequence_map: HashMap<u16, u16>,
-    rowid_id_map: HashMap<u16, u16>,
-}
-
-impl RegexSequenceTrait for DeviceBrandRegexSequence {
-    fn db(&self) -> &Option<Database> {
-        &self.db
-    }
-
-    fn name(&self) -> &String {
-        &self.name
-    }
-
-    fn rowid_sequence_map(&self) -> &HashMap<u16, u16> {
-        &self.rowid_sequence_map
-    }
-}
-
-impl DeviceBrandRegexSequence {
-    pub fn init<'a, R, I, S>(
-        &mut self,
-        rowids: I,
-        ids: I,
-        regexes: S,
-        sequences: I,
-        os_family_codes: S,
-        os_codes: S,
-    ) -> Result<()>
-    where
-        R: AsRef<str>,
-        I: Iterator<Item = &'a u16>,
-        S: Iterator<Item = R>,
-    {
-        let mut patterns = Vec::new();
-        let tup = rowids
-            .zip(ids)
-            .zip(regexes)
-            .zip(sequences)
-            .zip(os_family_codes)
-            .zip(os_codes);
-        tup.for_each(|(((((rowid, id), regex), seq), os_family_code), os_code)| {
-            let mut pattern = match Pattern::from_str(regex.as_ref()) {
-                Ok(ptrn) => ptrn,
-                Err(err) => {
-                    eprintln!("{:?}", err);
-                    return ();
-                }
-            };
-            pattern.id = Some(*rowid as usize);
-            patterns.push(pattern);
-
-            // add new entry for <id, sequence> map
-            self.rowid_sequence_map.insert(*rowid, *seq);
-
-            // add code entry for <id, word> map
-            let codes = [
-                os_family_code.as_ref().to_string(),
-                os_code.as_ref().to_string(),
-            ];
-            self.rowid_code_map.insert(*rowid, codes);
-            self.rowid_id_map.insert(*rowid, *id);
-        });
-
-        self.db = Some(Patterns::from(patterns).build()?);
-
-        Ok(())
-    }
-
-    pub fn get_id<'a, T, S>(
-        &self,
-        ua: &T,
-        scratch: &mut RegexSequenceScratch,
-        os_family_code: &S,
-        os_code: &S,
-    ) -> Result<Option<u16>>
-    where
-        T: AsRef<[u8]>,
-        S: AsRef<str>,
-    {
-        let id_seqs = self.get_ids(ua, scratch)?;
-
-        for item in &id_seqs {
-            let codes = match self.rowid_code_map.get(&item) {
-                None => continue,
-                Some(arr) => arr,
-            };
-
-            if os_family_code.as_ref().to_string() == codes[0]
-                && os_code.as_ref().to_string() == codes[1]
-            {
-                return match self.rowid_id_map.get(&item) {
-                    None => Ok(None),
-                    Some(id) => Ok(Some(*id)),
-                };
-            }
-        }
-
-        Ok(None)
     }
 }
 
@@ -439,43 +310,5 @@ mod tests {
             .unwrap();
 
         assert!(matches!(id, Some(_)));
-    }
-
-    #[test]
-    fn test_device_name_get_id() {
-        let mut regex_seq = DeviceBrandRegexSequence::new();
-
-        let rowids: Vec<u16> = vec![0, 1];
-        let ids: Vec<u16> = vec![1, 2];
-        let regexes = vec![
-            r"Mozilla.*Android.*; ([0-9a-z\.\_\-\/]+).*",
-            r"(iPhone|iPad|iTab|iPod)",
-        ];
-        let sequences: Vec<u16> = vec![10, 20];
-        let os_family_codes = vec!["android", "ios"];
-        let os_codes = vec!["-all-", "-all-"];
-
-        regex_seq
-            .init(
-                rowids.iter(),
-                ids.iter(),
-                regexes.iter(),
-                sequences.iter(),
-                os_family_codes.iter(),
-                os_codes.iter(),
-            )
-            .unwrap();
-
-        let mut scratch = regex_seq.alloc_scratch().unwrap();
-        let id = regex_seq
-            .get_id(
-                &"iPhone 12",
-                &mut scratch,
-                &String::from("ios"),
-                &String::from("-all-"),
-            )
-            .unwrap();
-
-        assert!(matches!(id, Some(expected) if expected == 2));
     }
 }
